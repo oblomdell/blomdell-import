9c1f3d0e3c786aff851b2fa11ec1633157cf5ce3
oblomdell <oblomdell@gmail.com>
2024-06-03 16:58:33 +0200

    Små fix

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,19 +1,22 @@
 <script>
     import "../app.css";
+    import logo from '$lib/assets/logo.jpeg'
 </script>
 
 <div class="border-8 border-blue-600">
 
-    <header class="pt-2 pb-8">
+    <header class="pt-2 pb-8 flex flex-col items-center">
 
-        <nav class="font-serif text-xs">
-            <ul class="flex justify-center gap-14">
+        <nav class="font-serif text-xs mb-5">
+            <ul class="flex gap-14">
                 <li><a href="#" class="focus:underline underline-offset-4">SYSTEMBOLAGET</a></li>
                 <li><a href="#" class="focus:underline underline-offset-4">RESTAURANGKUND</a></li>
                 <li><a href="#" class="focus:underline underline-offset-4">PRIVATKUND</a></li>
                 <li><a href="#" class="focus:underline underline-offset-4">KONTAKT</a></li>
             </ul>
         </nav>
+
+        <a href="#"><img class="max-w-full h-24" src={logo} alt="Blomdell Import logo" /></a>
 
     </header>
 
