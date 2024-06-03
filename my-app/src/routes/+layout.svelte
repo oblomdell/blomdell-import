c361ba7334e483c352ff3435004cb9ff8732701d
oblomdell <oblomdell@gmail.com>
2024-06-03 16:11:33 +0200

    Skapade ett Sveltekit projekt
    Satte upp projektet mha Sveltekit samt började på grundläggande layout.

---
+++ my-app/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+    import "../app.css";
+</script>
+
+<div class="border-8 border-blue-600">
+
+    <header class="pt-2 pb-8">
+
+        <nav class="font-serif text-xs">
+            <ul class="flex justify-center gap-14">
+                <li><a href="#" class="focus:underline underline-offset-4">SYSTEMBOLAGET</a></li>
+                <li><a href="#" class="focus:underline underline-offset-4">RESTAURANGKUND</a></li>
+                <li><a href="#" class="focus:underline underline-offset-4">PRIVATKUND</a></li>
+                <li><a href="#" class="focus:underline underline-offset-4">KONTAKT</a></li>
+            </ul>
+        </nav>
+
+    </header>
+
+    <slot />
+
+</div>
