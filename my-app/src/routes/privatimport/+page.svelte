6b329be918ce76b476ee7461ea5056e7e4f5505c
Oliver Blomdell <128600290+oblomdell@users.noreply.github.com>
2024-12-13 17:52:39 +0100

    Lade till fler bilder

--- my-app/src/routes/privatimport/+page.svelte
+++ my-app/src/routes/privatimport/+page.svelte
@@ -1,30 +1,73 @@
-<main class="max-w-screen-sm mx-auto">
-    <h1 class="text-2xl mb-5 text-center">Privatimport</h1>
-    <p>Vår tequila går enbart att beställa hem till din lokala butik via <a href="https://www.systembolaget.se/bestalla-och-handla/privatimport/" target="_blank" class="underline text-blue-950">privatimport</a>.</p>
-    <p class="mb-2">Våra viner kan man beställa direkt via systembolaget <a href="https://www.systembolaget.se/sortiment/?q=blomdell" target="_blank" class="underline text-blue-950">här</a>.</p>
-    <p>För att beställa via privatimport behöver du:</p>
-    <ul class="list-decimal pl-4 py-4">
-        <li>
-            <a href="#" target="_blank" class="underline text-blue-950">Skapa ett konto</a> på systembolaget.
-        </li>
-        <li>
-            När du är inloggad registrerar du en förfrågan <a href="https://www.systembolaget.se/bestalla-och-handla/privatimport/forfragan/" target="_blank" class="underline text-blue-950">via detta formulär</a>.
-        </li>
-        <li>
-            Fyll i formuläret:
-            <ul class="pl-8">
-                <li>Börja med att välj din butik.</li>
-                <li>Säljare: <strong>Blomdell C&M AB</strong></li>
-                <li>Lägg till dryck: <strong>använd namn som står på vår <a href="/produkter" class="underline text-blue-950">produktsida</a> (t.ex. "Skelly Anejo" eller "Talavera Blå")</strong></li>
-                <li>Typ av dryck: <strong>Spritdryck</strong></li>
-                <li>Förpackning: <strong>Flaska</strong></li>
-                <li>Volym ml/st: <strong>750</strong></li>
-                <li>Antal: <strong>Så många flaskor du vill beställa</strong></li>
-            </ul>
-        </li>
-        <li>
-            Skicka förfrågan.
-        </li>
-    </ul>
-    <p class="">Har du frågor kring beställningen? <a href="/kontakt" class="underline text-blue-950">Kontakta oss</a>.</p>
+<script>
+    import coverphoto from "$lib/assets/coverphotos/privatimport.jpg";
+</script>
+
+<main class="max-w-screen-xl mx-auto flex">
+    <section class="flex-col">
+        <h1 class="text-2xl mb-5 text-center">Privatimport</h1>
+        <p>
+            Vår tequila går enbart att beställa hem till din lokala butik via <a
+                href="https://www.systembolaget.se/bestalla-och-handla/privatimport/"
+                target="_blank"
+                class="underline text-blue-950">privatimport</a
+            >.
+        </p>
+        <p class="mb-2">
+            Våra viner kan man beställa direkt via systembolaget <a
+                href="https://www.systembolaget.se/sortiment/?q=blomdell"
+                target="_blank"
+                class="underline text-blue-950">här</a
+            >.
+        </p>
+        <p>För att beställa via privatimport behöver du:</p>
+        <ul class="list-decimal pl-4 py-4">
+            <li>
+                <a href="#" target="_blank" class="underline text-blue-950"
+                    >Skapa ett konto</a
+                > på systembolaget.
+            </li>
+            <li>
+                När du är inloggad registrerar du en förfrågan <a
+                    href="https://www.systembolaget.se/bestalla-och-handla/privatimport/forfragan/"
+                    target="_blank"
+                    class="underline text-blue-950">via detta formulär</a
+                >.
+            </li>
+            <li>
+                Fyll i formuläret:
+                <ul class="pl-8">
+                    <li>Börja med att välj din butik.</li>
+                    <li>Säljare: <strong>Blomdell C&M AB</strong></li>
+                    <li>
+                        Lägg till dryck: <strong
+                            >använd namn som står på vår <a
+                                href="/produkter"
+                                class="underline text-blue-950">produktsida</a
+                            > (t.ex. "Skelly Anejo" eller "Talavera Blå")</strong
+                        >
+                    </li>
+                    <li>Typ av dryck: <strong>Spritdryck</strong></li>
+                    <li>Förpackning: <strong>Flaska</strong></li>
+                    <li>Volym ml/st: <strong>750</strong></li>
+                    <li>
+                        Antal: <strong>Så många flaskor du vill beställa</strong
+                        >
+                    </li>
+                </ul>
+            </li>
+            <li>Skicka förfrågan.</li>
+        </ul>
+        <p class="">
+            Har du frågor kring beställningen? <a
+                href="/kontakt"
+                class="underline text-blue-950">Kontakta oss</a
+            >.
+        </p>
+    </section>
+
+    <img
+        class="max-w-full h-80 w-auto m-auto object-cover"
+        src={coverphoto}
+        alt="Three Skelly Azulejos bottles"
+    />
 </main>
