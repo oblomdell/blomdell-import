36f881eb70f6bef274452925f035595369a43268
Oliver Blomdell <128600290+oblomdell@users.noreply.github.com>
2025-03-18 12:33:04 +0100

    Update svelte.config.js

--- my-app/svelte.config.js
+++ my-app/svelte.config.js
@@ -1,10 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+import adapter from '@sveltejs/adapter-static';
+
+export default {
 	kit: {
-		adapter: adapter()
-	},
-	preprocess: vitePreprocess()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
 };
-export default config;
